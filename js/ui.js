3025c5c754c0fd61849c31ce08d3752361e5bb0d
maystrova <maystrovakate@gmail.com>
2016-09-30 17:49:18 +0300

    slider, catalog started

---
+++ js/ui.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('.sports-gallary').owlCarousel({
+        loop: true,
+        margin: 20,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+                nav: true
+            },
+            768: {
+                items: 4,
+                nav: false
+            },
+            992: {
+                items: 5,
+                nav: true,
+                loop: false
+            },
+            1200: {
+                items: 6,
+                nav: true,
+                loop: false
+            }
+        }
+    });
+    
